cc1d62b2984cd490069f37db8c2bb7ed8444d1ae
Rizwan H <144271165+Rizwan-Hossen@users.noreply.github.com>
2024-03-30 00:36:44 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head dangerouslySetInnerHTML={{ __html: `<!-- Devloped By Rizwan Hossen ft Code thousand 
+    Website: https://www.codethousand.net/ --> `}} />
       <body>
         <Main />
         <NextScript />
